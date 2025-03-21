30dbe22a2ec00876674a2c7150dee42e352dbf23
Victor Pacheco <victorpachecodearruda@gmail.com>
2025-03-21 15:08:29 -0300

    Adicionando validator e endpoint de criação

---
+++ Api/Migrations/20250321170658_AjusteNasColunas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteNasColunas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "titulo",
+                table: "Tb_Tasks",
+                newName: "Titulo");
+
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Tb_Tasks",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "descricao",
+                table: "Tb_Tasks",
+                newName: "Descricao");
+
+            migrationBuilder.RenameColumn(
+                name: "dataCriacao",
+                table: "Tb_Tasks",
+                newName: "DataCriacao");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Tb_Tasks",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Titulo",
+                table: "Tb_Tasks",
+                newName: "titulo");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Tb_Tasks",
+                newName: "status");
+
+            migrationBuilder.RenameColumn(
+                name: "Descricao",
+                table: "Tb_Tasks",
+                newName: "descricao");
+
+            migrationBuilder.RenameColumn(
+                name: "DataCriacao",
+                table: "Tb_Tasks",
+                newName: "dataCriacao");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "dataCriacao",
+                table: "Tb_Tasks",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
